be1d6d3758ae16deb3fc2724c4c7a78e4ef183c4
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-24 16:11:30 -0300

    teste 2

--- main.go
+++ main.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
     a := 10
     b := 5
-    fmt.Println(soma(a,b))
+    fmt.Println("soma:", soma(a,b))
+    fmt.Println("subtrair:", subtrair(a,b))
+    fmt.Println("multiplicar:", multiplicar(a,b))
+    fmt.Println("dividir:", dividir(a,b))
 }
